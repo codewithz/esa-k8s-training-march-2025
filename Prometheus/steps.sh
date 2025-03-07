e06a0fbb1860e62fa17cd0597e5fe872fe6df8da
Zartab Nakhwa <zartab@codewithz.com>
2025-03-07 20:16:08 +0530

    Check Deployments and StatefulSets

--- Prometheus/steps.sh
+++ Prometheus/steps.sh
@@ -10,3 +10,18 @@ kubectl get ds
 kubectl patch ds prometheus-prometheus-node-exporter --type "json" -p '[{"op": "remove", "path" : "/spec/template/spec/containers/0/volumeMounts/2/mountPropagation"}]'
 
 # Updatated formatting
+
+kubectl get deploy
+
+NAME                                  READY   UP-TO-DATE   AVAILABLE   AGE
+prometheus-grafana                    1/1     1            1           2m7s
+prometheus-kube-prometheus-operator   1/1     1            1           2m7s
+prometheus-kube-state-metrics         1/1     1            1           2m7s
+sample-node-app-deploy                3/3     3            3           23h
+
+
+kubectl get statefulset
+
+NAME                                                   READY   AGE
+alertmanager-prometheus-kube-prometheus-alertmanager   1/1     2m12s
+prometheus-prometheus-kube-prometheus-prometheus       1/1     2m12s
